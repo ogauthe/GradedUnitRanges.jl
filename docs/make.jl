05f471658a6a101136c3df32e2678e2593dc22e0
Matt Fishman <mtfishman@users.noreply.github.com>
2025-03-27 11:55:56 -0400

    Update template (#25)

--- docs/make.jl
+++ docs/make.jl
@@ -12,9 +12,9 @@ makedocs(;
   authors="ITensor developers <support@itensor.org> and contributors",
   sitename="GradedUnitRanges.jl",
   format=Documenter.HTML(;
-    canonical="https://ITensor.github.io/GradedUnitRanges.jl",
+    canonical="https://itensor.github.io/GradedUnitRanges.jl",
     edit_link="main",
-    assets=String[],
+    assets=["assets/favicon.ico", "assets/extras.css"],
   ),
   pages=["Home" => "index.md", "Reference" => "reference.md"],
 )
